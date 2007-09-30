942abb9eba0d95f685682d47fa363e7916910f2d
Denis Defreyne <denis.defreyne@stoneship.org>
2007-09-30 10:37:01 +0000

    converted Stoneship to use nanoc 1.6's post-layout filters
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/samples/stoneship%40386

--- lib/stoneship.rb
+++ lib/stoneship.rb
@@ -1,3 +1,14 @@
+# Make sure we're using nanoc 1.6 or higher
+
+if Nanoc::VERSION < '1.6'
+  puts 'WARNING:'
+  puts 'You need nanoc 1.6 or higher to compile this site. If you really want to'
+  puts 'compile this site with an older version of nanoc, set FORCE to true in your'
+  puts 'environment.'
+  puts
+  exit unless ENV['FORCE'] == 'true'
+end
+
 ########## NAVIGATION
 
 def link_to_unless_current(path, text)
@@ -15,6 +26,7 @@ def permalink(a_string)
 end
 
 class String
+
   def indent(amount, string="\t")
     is_on = true
     self.split("\n").collect do |line|
@@ -32,4 +44,5 @@ class String
       result
     end.join("\n")
   end
+
 end
