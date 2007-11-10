863538a97018df60ef244f80cd3b1e9c9ac7de91
Denis Defreyne <denis.defreyne@stoneship.org>
2007-11-10 16:50:03 +0000

    removed builtin stuff (nanoc doesn't support it anymore); updated old page[:path] syntax to page.path (etc); added markdown and smartypants filter (since nanoc 2.0 doesn't have it built-in anymore)
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/samples/stoneship%40484

--- lib/plugins/link_to_unless_current_plugin.rb
+++ lib/plugins/link_to_unless_current_plugin.rb
@@ -1,5 +1,5 @@
 def link_to_unless_current(path, text, params={})
-  if @page[:path] == path
+  if @page.path == path
     "<span class=\"active\">#{text}</span>"
   else
     "<a #{params.inject('') { |memo, (key, value)| memo + "#{key.to_s}=\"#{value.to_s}\" " } }href=\"#{path}\">#{text}</a>"
