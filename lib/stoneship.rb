0fcdebd0b76fef472daa04a5af5abba1c9b229a7
Denis Defreyne <denis.defreyne@stoneship.org>
2007-11-16 12:09:02 +0000

    fixed bug which would prevent nanoc 2.0 from compiling stoneship due to a broken version check
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/samples/stoneship%40510

--- lib/stoneship.rb
+++ lib/stoneship.rb
@@ -1,5 +1,5 @@
 # Make sure we're using nanoc 2.0 or higher
-if Nanoc::VERSION < '2.0.'
+if Nanoc::VERSION < '2.0'
   puts 'WARNING:'
   puts 'You need nanoc 1.7 or higher to compile this site. If you really want to'
   puts 'compile this site with an older version of nanoc, set FORCE to true in your'
