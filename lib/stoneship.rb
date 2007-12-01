e10aa991773edebae7f01bcab0daa4c3de6e2554
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-01 10:38:04 +0000

    it's nanoc 2.0, not nanoc 1.7
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/samples/stoneship%40545

--- lib/stoneship.rb
+++ lib/stoneship.rb
@@ -1,7 +1,7 @@
 # Make sure we're using nanoc 2.0 or higher
 if Nanoc::VERSION < '2.0'
   puts 'WARNING:'
-  puts 'You need nanoc 1.7 or higher to compile this site. If you really want to'
+  puts 'You need nanoc 2.0 or higher to compile this site. If you really want to'
   puts 'compile this site with an older version of nanoc, set FORCE to true in your'
   puts 'environment.'
   puts
