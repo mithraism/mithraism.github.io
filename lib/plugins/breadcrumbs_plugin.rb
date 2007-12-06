f02b037b6fa35ed4ad76f9beef2e0d353d68607f
Denis Defreyne <denis.defreyne@stoneship.org>
2007-12-06 14:09:04 +0000

    updated breadcrumbs implementation to use new parent-child links
    --HG--
    extra : convert_revision : svn%3A177060d3-6165-4ab0-95eb-642399a075a8/samples/stoneship%40568

--- lib/plugins/breadcrumbs_plugin.rb
+++ lib/plugins/breadcrumbs_plugin.rb
@@ -3,14 +3,15 @@ def breadcrumbs(params={})
   separator = params[:separator] || ' > '
   root      = params[:root]      || 'Home'
 
-  # Find page paths
-  memo = []
-  trail_paths = @page.path.sub(/^\//, '').split('/').map { |part| memo << part; '/' + memo.join('/') + '/' }
+  stack = []
 
-  # Find pages
-  trail_pages = trail_paths.map { |path| @pages.select { |p| p.path == path }.first }
-  trail_hashes = [ { :title => root, :path => '/' } ] + trail_pages.map { |p| { :title => p.title, :path => p.path } }
+  # Build stack
+  current = @page
+  until current.nil? do
+    stack << current
+    current = current.parent
+  end
 
-  # Convert to links and join
-  trail_hashes.map { |p| link_to_unless_current(p[:path], h(p[:title])) }.join(separator)
+  # Convert to text
+  stack.reverse.map { |page| link_to_unless_current(page.path, h(page.parent.nil? ? root : page.title)) }.join(separator)
 end
