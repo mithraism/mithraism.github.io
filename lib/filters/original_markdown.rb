0562dd07506690d3563734daa4f772156a6ab727
Denis Defreyne <denis.defreyne@stoneship.org>
2009-02-03 09:25:36 +0100

    adding original_markdown filter

---
+++ lib/filters/original_markdown.rb
@@ -0,0 +1,17 @@
+module Nanoc::Filters
+
+  class OriginalMarkdown < Nanoc::Filter
+
+    identifier :original_markdown
+
+    def run(content)
+      open('|perl /usr/local/lib/Markdown.pl', 'r+') do |io|
+        io.write(content)
+        io.close_write
+        io.read
+      end
+    end
+
+  end
+
+end
