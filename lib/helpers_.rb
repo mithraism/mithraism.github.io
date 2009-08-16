f1ac1b67f35d3f7a5a47fda5ff11a6ed4b8c9c76
Denis Defreyne <denis.defreyne@stoneship.org>
2009-08-16 12:06:02 +0200

    corrected name of included Rendering helper
    --HG--
    branch : nanoc-3.0.x

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -2,7 +2,7 @@
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::HTMLEscape
-include Nanoc3::Helpers::Render
+include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::XMLSitemap
 
 # Helpers - custom
