64c9241994a5cd543ecb9669612069da821c6c4d
Denis Defreyne <denis.defreyne@stoneship.org>
2009-11-02 12:35:47 +0100

    changed requirement + message to nanoc 3.0
    --HG--
    branch : nanoc-3.0.x

--- lib/stoneship.rb
+++ lib/stoneship.rb
@@ -1,7 +1,7 @@
-# Make sure we're using nanoc 2.1 or higher
+# Make sure we're using nanoc 3.0 or higher
 if Nanoc3::VERSION < '3.0'
   puts 'WARNING:'
-  puts 'You need nanoc 2.1 or higher to compile this site. If you really ' +
+  puts 'You need nanoc 3.0 or higher to compile this site. If you really ' +
        'want to compile this site with an older version of nanoc, set ' +
        'FORCE to true in your environment.'
 
