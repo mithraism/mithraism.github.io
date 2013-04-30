a3b2f14b9f5a82e7fe90d2fc8a23d722b3c2e477
Denis Defreyne <denis.defreyne@stoneship.org>
2013-04-30 22:59:43 +0200

    Require nanoc 4.0

--- lib/_stoneship.rb
+++ lib/_stoneship.rb
@@ -1,7 +1,7 @@
-# Make sure we're using nanoc 3.0 or higher
-if Nanoc3::VERSION < '3.0'
+# Make sure we're using nanoc 4.0 or higher
+if Nanoc::VERSION < '4.0'
   puts 'WARNING:'
-  puts 'You need nanoc 3.0 or higher to compile this site. If you really ' +
+  puts 'You need nanoc 4.0 or higher to compile this site. If you really ' +
        'want to compile this site with an older version of nanoc, set ' +
        'FORCE to true in your environment.'
 
